514e3a1a1e29a85432eb13b0afc2b0a44a350f09
Seth Coussens <seth.coussens@gmail.com>
2017-11-16 08:24:05 -0600

    resolved ts erros

--- CalorieCounter/src/index.ts
+++ CalorieCounter/src/index.ts
@@ -1,13 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Alexa = require("alexa-sdk");
-var APP_ID = undefined;
-var SKILL_NAME = 'Space Facts';
-var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
-var HELP_REPROMPT = 'What can I help you with?';
-var STOP_MESSAGE = 'Goodbye!';
-var data = [
+import * as Alexa from 'alexa-sdk';
+import { LaunchRequest } from 'alexa-sdk';
+
+const APP_ID = undefined;
+const SKILL_NAME = 'Space Facts';
+const GET_FACT_MESSAGE = "Here's your fact: ";
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const data = [
     'A year on Mercury is just 88 days long.',
     'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
     'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
@@ -22,40 +23,43 @@ var data = [
     'The temperature inside the Sun can reach 15 million degrees Celsius.',
     'The Moon is moving approximately 3.8 cm away from our planet every year.',
 ];
-exports.index = function (context, req) {
+
+export const index = (context: any, req: any) => {
     context.log(JSON.stringify(req, null, 2));
-    var request = req.body;
-    var ctx = req.body.context;
-    var alexa = Alexa.handler(request, ctx);
+
+    let request = req.body as Alexa.RequestBody<{}>;
+    let ctx = req.body.context;
+    const alexa = Alexa.handler(request, ctx);
     alexa.appId = '';
     alexa.registerHandlers(handlers);
     alexa.execute();
-};
-var handlers = {
+}
+
+const handlers: Alexa.Handlers<{}> = {
     'LaunchRequest': function () {
-        var self = this;
+        let self: Alexa.Handler<{}> = this;
         self.emit('GetNewFactIntent');
     },
     'GetNewFactIntent': function () {
-        var self = this;
-        var factArr = data;
-        var factIndex = Math.floor(Math.random() * factArr.length);
-        var randomFact = factArr[factIndex];
-        var output = GET_FACT_MESSAGE + randomFact;
+        let self: Alexa.Handler<{}> = this;
+        const factArr = data;
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+        const output = GET_FACT_MESSAGE + randomFact;
         self.emit(':tellWithCard', output, ':responseReady', output);
     },
     'AMAZON.HelpIntent': function () {
-        var self = this;
-        var speechOutput = HELP_MESSAGE;
-        var reprompt = HELP_REPROMPT;
+        let self: Alexa.Handler<{}> = this;
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
         self.emit(':responseReady', HELP_MESSAGE);
     },
     'AMAZON.CancelIntent': function () {
-        var self = this;
+        let self: Alexa.Handler<{}> = this;
         self.emit(':responseReady', STOP_MESSAGE);
     },
     'AMAZON.StopIntent': function () {
-        var self = this;
+        let self: Alexa.Handler<{}> = this;
         self.emit(':responseReady', STOP_MESSAGE);
     },
 };
