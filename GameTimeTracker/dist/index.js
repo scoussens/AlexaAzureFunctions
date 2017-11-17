b1c31a10066987a7779ff1d606eb08870cb44fa3
Seth Coussens <seth.coussens@gmail.com>
2017-11-17 16:14:59 -0600

    added basic intent responses

--- GameTimeTracker/dist/index.js
+++ GameTimeTracker/dist/index.js
@@ -7,7 +7,29 @@ exports.index = (context, req) => {
     const awsContext = new azure_alexa_mock_context_1.Context('GameTimeTracker', context);
     let alexa = Alexa.handler(req.body, awsContext);
     alexa.appId = 'amzn1.ask.skill.5c2a5512-6992-4574-b57b-5c8c89989e87';
-    let handlers = {};
+    let handlers = {
+        'LaunchRequest': function () {
+            this.emit('StartTimerIntent');
+        },
+        'StartTimerIntent': function () {
+            this.emit(':tell', 'Timer started!');
+        },
+        'StopTimerIntent': function () {
+            this.emit(':tell', 'Timer stopped! You have x time left.');
+        },
+        'CheckAvailableTimeIntent': function () {
+            this.emit(':tell', 'You have x minutes available to play.');
+        },
+        'AMAZON.CancelIntent': function () {
+            this.emit('StopTimerIntent');
+        },
+        'AMAZON.HelpIntent': function () {
+            this.emit(':tell', 'This skill is used to track daily game play time. You can check how much time is remaining each day, as well as start and stop the timer.');
+        },
+        'AMAZON.StopIntent': function () {
+            this.emit('StopTimerIntent');
+        }
+    };
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
--- GameTimeTracker/src/index.ts
+++ GameTimeTracker/src/index.ts
@@ -9,7 +9,27 @@ export const index = (context: HttpContext, req: HttpRequest) => {
     let alexa = Alexa.handler(req.body, awsContext);
     alexa.appId = 'amzn1.ask.skill.5c2a5512-6992-4574-b57b-5c8c89989e87';
     let handlers: Alexa.Handlers<{}> = {
-
+        'LaunchRequest': function () {
+            this.emit('StartTimerIntent');
+        },
+        'StartTimerIntent': function () {
+            this.emit(':tell', 'Timer started!');
+        },
+        'StopTimerIntent': function () {
+            this.emit(':tell', 'Timer stopped! You have x time left.');
+        },
+        'CheckAvailableTimeIntent': function () {
+            this.emit(':tell', 'You have x minutes available to play.');
+        },
+        'AMAZON.CancelIntent': function () {
+            this.emit('StopTimerIntent');
+        },
+        'AMAZON.HelpIntent': function () {
+            this.emit(':tell', 'This skill is used to track daily game play time. You can check how much time is remaining each day, as well as start and stop the timer.');
+        },
+        'AMAZON.StopIntent': function () {
+            this.emit('StopTimerIntent');
+        }
     }
     alexa.registerHandlers(handlers);
     alexa.execute();
